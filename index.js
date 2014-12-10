b598d6f4e2c761e6d4c354a885962de6874d50a1
jasonamartin <jmartin@jasonamartin.com>
2014-12-09 19:09:39 -0800

    Continued evolution to node module

--- index.js
+++ index.js
@@ -10,61 +10,50 @@
  * @return {String}
  */
 
+var fs = require('fs');
+
 module.exports = {
-	wowRep: function(player,server) {
 
-		/**
-		 Reputation levels in WOW:
-		 0: hated
-		 1: hostile
-		 2: unfriendly
-		 3: neutral
-		 4: friendly
-		 5: honored
-		 6: revered
-		 7: exalted
-		 **/
+	init: function(server,player){
+		var opts = {
+			'server': server,
+			'player': player
+		};
 
-		//create a reputation object. Key is rep name and value is the target div
-		var reputation = {
-			"hated" : $("#hated"),
-			"hostile" : $("#hostile"),
-			"unfriendly" : $("#unfriendly"),
-			"neutral" : $("#neutral"),
-			"friendly" : $("#friendly"),
-			"honored" : $("#honored"),
-			"revered" : $("#revered"),
-			"exalted" : $("#exalted")
-		}
+		var moduleOptions = JSON.stringify(opts);
 
-		var reputationIndex = ["hated","hostile","unfriendly","neutral","friendly","honored","revered","exalted"];
 
-		return $.ajax({
-			url: 'http://us.battle.net/api/wow/character/'+server+'/'+player+'?fields=reputation',
-			dataType: 'JSONP',
-			jsonpCallback: 'callback',
-			type: 'GET',
-			jsonp: 'jsonp',
-			success: function (data) {
-				var targetElement;
-				console.log("Daylight's burning");
-				//loop through reponse data
-				for (var i = 0, len = data.reputation.length; i < len; i++) {
-					//assisgn to content var for ease
-					var content = data.reputation[i]; //node has id, name, standing, value and max
-					//create div for this listing and give it a class name for styling
-					var listingElement = document.createElement("div");
-					listingElement.className = "rep-listing";
-					//let's have fun and add a classes for special rep standings & set the target div while we're at it
-					targetElement = reputation[reputationIndex[content.standing]];
-					//create a text node with desired content, name in this case
-					var listingElementContent = document.createTextNode(content.name);
-					//append the desired content to the appropriate div
-					listingElement.appendChild(listingElementContent);
-					//insert into the rep status target div
-					targetElement[0].appendChild(listingElement);
-				}
+		fs.writeFile('./config.json', moduleOptions, function (err) {
+			if (err) {
+				console.log('The config file was not saved.');
+				console.log(err.message);
+				return;
 			}
+			console.log('Config file updated.');
 		});
+
+
+	},
+
+	loadConfig: function(){
+		var configData = fs.readFileSync('./config.json'),
+			myData;
+		try {
+			myData = JSON.parse(configData);
+			console.dir(myData);
+		}
+		catch (err) {
+			console.log('The JSON was not parsed properly.')
+			console.log(err);
+		}
+	},
+	wowRep: function(player,server) {
+		//coming back soon
+	},
+
+	demo: function(){
+		init("Gor","Uther");
+		var data = loadConfig();
+		console.log(data);
 	}
 };
